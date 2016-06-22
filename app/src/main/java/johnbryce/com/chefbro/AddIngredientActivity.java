fe7136ff4dc012a40d1100bf42860443bc684df9
EthanHemo <ethan.hemo@gmail.com>
2016-06-22 23:15:38 +0300

    Add autocomplete textview to ingredients activity

--- app/src/main/java/johnbryce/com/chefbro/AddIngredientActivity.java
+++ app/src/main/java/johnbryce/com/chefbro/AddIngredientActivity.java
@@ -2,12 +2,52 @@ package johnbryce.com.chefbro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddIngredientActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference ref;
+    AutoCompleteTextView actvCatgory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ingredient);
+
+        actvCatgory = (AutoCompleteTextView)findViewById(R.id.AutoCompleteIngredientCategory);
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("/IngredientCategories");
+        ValueEventListener postListener = new ValueEventListener(){
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(AddIngredientActivity.this, "Error getting categiry data: "+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<String> categories = new ArrayList<>();
+                for(DataSnapshot data: dataSnapshot.getChildren())
+                {
+                    IngredientCategory temp = data.getValue(IngredientCategory.class);
+                    categories.add(temp.getName());
+                }
+                //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,countries);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,categories);
+                actvCatgory.setAdapter(adapter);
+            }
+        };
+
     }
 }
